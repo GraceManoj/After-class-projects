c434b367a22c3052544ff8dbbdddc16757b5b487
Grace Manoj <helvimanoj@gmail.com>
2025-09-03 16:35:26 +0300

    co

---
+++ project 24/script.js
@@ -0,0 +1,6 @@
+function myFunction(){
+    document.getElementById("result").innerHTML=10*10;
+    document.getElementById("demo").innerHTML.firstChild.nodeValue;
+    document.getElementById("result1").innerHTML=10*4;
+    document.getElementById("demo").childNodes[0].nodeValue;
+}
